1d68962d11844c550fcc5e52af3316b968f891b3
juliney21 <55243931+juliney21@users.noreply.github.com>
2019-09-19 20:31:13 +0800

    areaABC

---
+++ Exercise1.java
@@ -0,0 +1,15 @@
+public class Exercise1{
+
+    public static void main(String[] args){
+
+        int A=3*6/2, B=3*4/2, C=4*3, total;
+
+        total=A+B+C;
+
+        System.out.println("A : "+ A);
+        System.out.println("B : "+ B);
+        System.out.println("C : "+ C);
+        System.out.println("\nTotal : "+ total);
+
+    }
+}
